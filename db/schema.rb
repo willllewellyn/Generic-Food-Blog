d8a4ca3d39a4f4cbb85d1bd793dcc76b825d4ec0
willllewellyn <william.llewellyn@outlook.com>
2018-10-14 22:18:44 +0100

    Add about section

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,49 +12,47 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_02_194554) do
-
-  create_table "article_comments", force: :cascade do |t|
-    t.string "commenter"
-    t.text "body"
-    t.integer "article_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["article_id"], name: "index_article_comments_on_article_id"
+ActiveRecord::Schema.define(version: 20_181_002_194_554) do
+  create_table 'article_comments', force: :cascade do |t|
+    t.string 'commenter'
+    t.text 'body'
+    t.integer 'article_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['article_id'], name: 'index_article_comments_on_article_id'
   end
 
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'articles', force: :cascade do |t|
+    t.string 'title'
+    t.text 'text'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "comments", force: :cascade do |t|
-    t.string "commenter"
-    t.text "body"
-    t.integer "article_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["article_id"], name: "index_comments_on_article_id"
+  create_table 'comments', force: :cascade do |t|
+    t.string 'commenter'
+    t.text 'body'
+    t.integer 'article_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['article_id'], name: 'index_comments_on_article_id'
   end
 
-  create_table "recipe_comments", force: :cascade do |t|
-    t.string "commenter"
-    t.text "body"
-    t.integer "recipe_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["recipe_id"], name: "index_recipe_comments_on_recipe_id"
+  create_table 'recipe_comments', force: :cascade do |t|
+    t.string 'commenter'
+    t.text 'body'
+    t.integer 'recipe_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['recipe_id'], name: 'index_recipe_comments_on_recipe_id'
   end
 
-  create_table "recipes", force: :cascade do |t|
-    t.string "title"
-    t.string "author"
-    t.text "ingredient"
-    t.text "method"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'recipes', force: :cascade do |t|
+    t.string 'title'
+    t.string 'author'
+    t.text 'ingredient'
+    t.text 'method'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
