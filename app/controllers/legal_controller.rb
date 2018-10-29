de18b3171b3630db0908606764997f4f810114b5
Will Llewellyn <william.llewellyn@outlook.com>
2018-10-29 13:38:24 +0000

    Adding footer and legal (#7)
    * Add about section
    
    * Added more legal
    
    * CSS fixes
    
    * CSS footer chnges

---
+++ app/controllers/legal_controller.rb
@@ -0,0 +1,5 @@
+class LegalController < ApplicationController
+  def privacy_policy; end
+
+  def terms_of_use; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'legal/privacy_policy'
+  get 'legal/terms_of_use'
+  get 'about/contacts'
+  get 'about/other_contacts'
+  get 'about/health_and_safety'
   get 'homepage/index'
 
   resources :articles do
