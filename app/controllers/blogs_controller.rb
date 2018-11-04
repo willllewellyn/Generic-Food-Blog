32c11d133026f068aa73c3fe7350afe30ea82849
willllewellyn <william.llewellyn@outlook.com>
2018-11-04 22:06:10 +0000

    Added blogs

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,52 @@
+class BlogsController < ApplicationController
+  http_basic_authenticate_with name: 'dhh', password: 'secret', except: %i[index show]
+
+  def index
+    @blogs = Blog.all
+  end
+
+  def show
+    @blog = Blog.find(params[:id])
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def edit
+    @blog = Blog.find(params[:id])
+  end
+
+  def create
+    @blog = Blog.new(blog_params)
+
+    if @blog.save
+      redirect_to @blog
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @blog = Blog.find(params[:id])
+
+    if @blog.update(blog_params)
+      redirect_to @blog
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @blog = Blog.find(params[:id])
+    @blog.destroy
+
+    redirect_to blogs_path
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title, :text)
+  end
+end
