3e0aaed768d08828abbddb2fbf46466ca45d189e
willllewellyn <william.llewellyn@outlook.com>
2018-11-16 23:15:08 +0000

    Added recipes and small CSS fix

--- app/controllers/their_recipes_controller.rb
+++ app/controllers/their_recipes_controller.rb
@@ -3,7 +3,15 @@
 class TheirRecipesController < ApplicationController
   def index; end
 
-  def chocolate_cake; end
+  def asparagus_avocado_sushi; end
 
   def baked_potatoes; end
+
+  def blueberry_vanilla_cake; end
+
+  def chocolate_cake; end
+
+  def fruit_clafoutis; end
+
+  def injera; end
 end
