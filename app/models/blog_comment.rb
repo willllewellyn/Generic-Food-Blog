4d85dc1b22ec81ea0dce6c479c119c292b777a05
willllewellyn <william.llewellyn@outlook.com>
2018-11-16 15:51:43 +0000

    Added comment validation to views

--- app/models/blog_comment.rb
+++ app/models/blog_comment.rb
@@ -2,4 +2,8 @@
 
 class BlogComment < ApplicationRecord
   belongs_to :blog
+  validates :commenter, presence: true,
+                    length: { minimum: 5, maximum: 50 }
+  validates :body, presence: true,
+                    length: { minimum: 5, maximum: 255 }
 end
--- app/models/recipe_comment.rb
+++ app/models/recipe_comment.rb
@@ -2,4 +2,8 @@
 
 class RecipeComment < ApplicationRecord
   belongs_to :recipe
+  validates :commenter, presence: true,
+                    length: { minimum: 5, maximum: 50 }
+  validates :body, presence: true,
+                    length: { minimum: 5, maximum: 255 }
 end
