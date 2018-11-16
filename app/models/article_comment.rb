faf3678e3ec36ba2d3bc41462b480495e7f4f603
willllewellyn <william.llewellyn@outlook.com>
2018-11-16 15:51:43 +0000

    Added comment validation to views

--- app/models/article_comment.rb
+++ app/models/article_comment.rb
@@ -2,4 +2,8 @@
 
 class ArticleComment < ApplicationRecord
   belongs_to :article
+  validates :commenter, presence: true,
+                    length: { minimum: 5, maximum: 50 }
+  validates :body, presence: true,
+                    length: { minimum: 5, maximum: 255 }
 end
