5069e109bc74e0d9dfcda7a26cd7beec115cd17b
willllewellyn <william.llewellyn@outlook.com>
2018-11-17 14:39:21 +0000

    Added recipe content

--- app/controllers/their_recipes_controller.rb
+++ app/controllers/their_recipes_controller.rb
@@ -7,11 +7,43 @@ class TheirRecipesController < ApplicationController
 
   def baked_potatoes; end
 
+  def baked_scotch_eggs; end
+
+  def beef_tenderloin_steaks_with_celert_root_gratin; end
+
   def blueberry_vanilla_cake; end
+
+  def bouillabaisse; end
+
+  def braised_lamb_with_herb_scented_jus; end
+
+  def cassoulet_with_duck_confit; end
+
+  def chicken_noodle_soup; end
 
   def chocolate_cake; end
 
+  def chocolate_macarons; end
+
+  def cream_cheese_and_jalapeno_stuffed_chicken_breast; end
+
+  def dinner_rolls; end
+
   def fruit_clafoutis; end
 
+  def grilled_oysters; end
+
+  def honey_tamarind_baby_back_ribs; end
+
   def injera; end
+
+  def over_the_top_mushroom_quiche; end
+
+  def pan_roast_salmon_with_tomato_viniagrette; end
+
+  def roast_chicken_with_aromatic_jus; end
+
+  def roast_leg_of_lamb; end
+
+  def slow_roasted_pork_loin_with_molasses_and_balsamic_glaze; end
 end
