5752741f09bf1faf7b9c8e83780dc9b217370b25
willllewellyn <william.llewellyn@outlook.com>
2018-11-17 21:56:41 +0000

    Added their recipe content

--- app/controllers/their_recipes_controller.rb
+++ app/controllers/their_recipes_controller.rb
@@ -13,6 +13,8 @@ class TheirRecipesController < ApplicationController
 
   def beef_stew_in_red_wine_sauce; end
 
+  def beef_stew_with_dumplings; end
+
   def beef_tenderloin_steaks_with_celert_root_gratin; end
 
   def bibimbap; end
@@ -23,7 +25,11 @@ class TheirRecipesController < ApplicationController
 
   def braised_lamb_with_herb_scented_jus; end
 
+  def caramelised_onions; end
+
   def cassoulet_with_duck_confit; end
+
+  def cheese_and_ham_omelette; end
 
   def chicken_noodle_soup; end
 
@@ -33,17 +39,25 @@ class TheirRecipesController < ApplicationController
 
   def cream_cheese_and_jalapeno_stuffed_chicken_breast; end
 
+  def devonshire_honey_cake; end
+
   def dinner_rolls; end
 
   def fruit_clafoutis; end
 
   def grilled_oysters; end
 
+  def honey_and_mustard_roast_gammon; end
+
   def honey_tamarind_baby_back_ribs; end
 
   def injera; end
 
+  def lembas_bread; end
+
   def mar_bar_cake; end
+
+  def marshmallow_crispie_bars; end
 
   def over_the_top_mushroom_quiche; end
 
@@ -51,7 +65,11 @@ class TheirRecipesController < ApplicationController
 
   def pan_roast_salmon_with_tomato_viniagrette; end
 
+  def pancakes; end
+
   def potato_gnocchi_with_mushroom_ragu; end
+
+  def potato_wedges; end
 
   def pull_apart_flauta_ring; end
 
@@ -62,4 +80,6 @@ class TheirRecipesController < ApplicationController
   def slow_roasted_pork_loin_with_molasses_and_balsamic_glaze; end
 
   def spicy_italian_pizza; end
+
+  def welsh_rarebit; end
 end
