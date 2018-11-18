fb97e739dbf4a80da182926a805df292794d620e
willllewellyn <william.llewellyn@outlook.com>
2018-11-18 15:35:12 +0000

    Added their recipe content

--- app/controllers/their_recipes_controller.rb
+++ app/controllers/their_recipes_controller.rb
@@ -7,6 +7,10 @@ class TheirRecipesController < ApplicationController
 
   def asparagus_avocado_sushi; end
 
+  def bacon_egg_and_spinach_freid_rice; end
+
+  def bacon_herb_and_cheese_star_bread; end
+
   def baked_potatoes; end
 
   def baked_scotch_eggs; end
@@ -25,9 +29,13 @@ class TheirRecipesController < ApplicationController
 
   def braised_lamb_with_herb_scented_jus; end
 
+  def caramel_cookies; end
+
   def caramelised_onions; end
 
   def cassoulet_with_duck_confit; end
+
+  def chcken_pesto_pene_with_bacon_and_feta; end
 
   def cheese_and_ham_omelette; end
 
@@ -39,25 +47,45 @@ class TheirRecipesController < ApplicationController
 
   def cream_cheese_and_jalapeno_stuffed_chicken_breast; end
 
+  def creamy_scallop_fettuccine_with_lemon_and_capers; end
+
+  def creamy_tomato_soup; end
+
   def devonshire_honey_cake; end
 
   def dinner_rolls; end
 
+  def firecracker_shrimp; end
+
   def fruit_clafoutis; end
+
+  def garlic_butter_steak_and_potatoes; end
+
+  def grilled_chicken_salad_with_cabbage; end
 
   def grilled_oysters; end
 
   def honey_and_mustard_roast_gammon; end
 
+  def honey_mustard_chicken_thighs_with_argula; end
+
   def honey_tamarind_baby_back_ribs; end
+
+  def hunters_sausage_and_sauerkraut_stew; end
 
   def injera; end
 
   def lembas_bread; end
 
+  def lemon_pie; end
+
   def mar_bar_cake; end
 
   def marshmallow_crispie_bars; end
+
+  def moroccan_braised_chicken; end
+
+  def mussels_and_clams_in_a_spicy_tomato_broth; end
 
   def over_the_top_mushroom_quiche; end
 
@@ -66,6 +94,8 @@ class TheirRecipesController < ApplicationController
   def pan_roast_salmon_with_tomato_viniagrette; end
 
   def pancakes; end
+
+  def parmesan_and_garlic_crusted_potato_wedges; end
 
   def potato_gnocchi_with_mushroom_ragu; end
 
@@ -77,9 +107,19 @@ class TheirRecipesController < ApplicationController
 
   def roast_leg_of_lamb; end
 
+  def russian_honey_cake; end
+
+  def samoa_cookie_inspired_iced_coffee; end
+
+  def seared_scallops_in_a_cream_lemon_caper_sauce; end
+
   def slow_roasted_pork_loin_with_molasses_and_balsamic_glaze; end
 
   def spicy_italian_pizza; end
+
+  def spinache_and_artichoke_dip_stuffed_garlic_bread; end
+
+  def trout_with_garlic_lemon_butter_herb_sauce; end
 
   def welsh_rarebit; end
 end
