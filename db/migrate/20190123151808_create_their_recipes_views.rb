cf6a5d6e4c97061a9f1cca0e79f878ea027f69ae
Will Llewellyn <william.llewellyn@outlook.com>
2019-01-23 15:41:40 +0000

    Added Their Recipes logging

---
+++ db/migrate/20190123151808_create_their_recipes_views.rb
@@ -0,0 +1,11 @@
+class CreateTheirRecipesViews < ActiveRecord::Migration[5.2]
+  def change
+    create_table :their_recipes_views do |t|
+      t.string :user_id
+      t.integer :page_id
+      t.string :page_name
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_22_164847) do
+ActiveRecord::Schema.define(version: 2019_01_23_151808) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,6 +85,14 @@ ActiveRecord::Schema.define(version: 2019_01_22_164847) do
     t.string "author"
     t.text "ingredient"
     t.text "method"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "their_recipes_views", force: :cascade do |t|
+    t.string "user_id"
+    t.integer "page_id"
+    t.string "page_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
