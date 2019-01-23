606d8893b97b96d9c98861c84db4cf2ea79bf5d4
Will Llewellyn <william.llewellyn@outlook.com>
2019-01-23 17:54:23 +0000

    Rubocop

--- db/migrate/20190123173251_add_page_name_to_article_view.rb
+++ db/migrate/20190123173251_add_page_name_to_article_view.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddPageNameToArticleView < ActiveRecord::Migration[5.2]
   def change
     add_column :article_views, :page_name, :string
--- db/migrate/20190123173617_add_page_name_to_blog_view.rb
+++ db/migrate/20190123173617_add_page_name_to_blog_view.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddPageNameToBlogView < ActiveRecord::Migration[5.2]
   def change
     add_column :blog_views, :page_name, :string
