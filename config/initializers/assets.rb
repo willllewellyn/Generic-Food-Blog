97833c8c1f42a023b185b1508f3f1dc24e7dd719
Will Llewellyn <william.llewellyn@outlook.com>
2019-01-31 11:25:47 +0000

    Possible chartkick fix

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -17,3 +17,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 Rails.application.config.assets.precompile += %w( chartkick.js )
 Rails.application.config.assets.precompile += %w( Chart.bundle.js )
+Rails.application.config.assets.precompile += %w( Chart.bundle )
