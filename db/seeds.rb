fe6a282ed83b26376692216ccf988d7d9f781248
Will Llewellyn <william.llewellyn@outlook.com>
2019-02-01 10:54:33 +0000

    Rubocop changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 TheirRecipesView.create!([
-  {user_id: "f6422a86ea416ad731c9d5509521ed5a", page_id: 17, page_name: "Chicken pesto pene with bacon and feta"},
-  {user_id: "f6422a86ea416ad731c9d5509521ed5a", page_id: 17, page_name: "Chicken pesto pene with bacon and feta"},
-  {user_id: "f6422a86ea416ad731c9d5509521ed5a", page_id: 39, page_name: "Mars bar cake"},
-  {user_id: "f6422a86ea416ad731c9d5509521ed5a", page_id: 39, page_name: "Mars bar cake"},
-  {user_id: "f6422a86ea416ad731c9d5509521ed5a", page_id: 39, page_name: "Mars bar cake"},
-  {user_id: "f6422a86ea416ad731c9d5509521ed5a", page_id: 3, page_name: "Bacon egg and spinach fried rice"},
-  {user_id: "f6422a86ea416ad731c9d5509521ed5a", page_id: 4, page_name: "Bacon herb and cheese star bread"}
-])
+                           { user_id: 'f6422a86ea416ad731c9d5509521ed5a', page_id: 17, page_name: 'Chicken pesto pene with bacon and feta' },
+                           { user_id: 'f6422a86ea416ad731c9d5509521ed5a', page_id: 17, page_name: 'Chicken pesto pene with bacon and feta' },
+                           { user_id: 'f6422a86ea416ad731c9d5509521ed5a', page_id: 39, page_name: 'Mars bar cake' },
+                           { user_id: 'f6422a86ea416ad731c9d5509521ed5a', page_id: 39, page_name: 'Mars bar cake' },
+                           { user_id: 'f6422a86ea416ad731c9d5509521ed5a', page_id: 39, page_name: 'Mars bar cake' },
+                           { user_id: 'f6422a86ea416ad731c9d5509521ed5a', page_id: 3, page_name: 'Bacon egg and spinach fried rice' },
+                           { user_id: 'f6422a86ea416ad731c9d5509521ed5a', page_id: 4, page_name: 'Bacon herb and cheese star bread' }
+                         ])
