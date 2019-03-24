1ab7bd08683055fd1b0f56c142db826fd5440c78
Will Llewellyn <william.llewellyn@outlook.com>
2019-03-24 21:20:34 +0000

    Added support for exporting multiple CSVs

--- app/models/their_recipes_view.rb
+++ app/models/their_recipes_view.rb
@@ -2,4 +2,16 @@
 
 class TheirRecipesView < ApplicationRecord
   attr_accessor :UserId, :PageId, :PageName
+
+  def self.to_csv
+    attributes = %w[id user_id page_id page_name created_at updated_at]
+
+    CSV.generate(headers: true) do |csv|
+      csv << attributes
+
+      all.each do |their_recipes_top10|
+        csv << attributes.map { |attr| their_recipes_top10.send(attr) }
+      end
+    end
+  end
 end
